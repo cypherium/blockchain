cb649b7027b4f9f4b62ed00491b0a7645858b873
Sicong Zhuang <sicong.zhuang@bsc.es>
2018-04-16 14:54:15 +0200

    adding headers

--- cypherium/cypherium.go
+++ cypherium/cypherium.go
@@ -1,14 +1,24 @@
-package cypherium
-
 /*
-The `NewProtocol` method is used to define the protocol and to register
-the handlers that will be called if a certain type of message is received.
-The handlers will be treated according to their signature.
+ * Copyright (C) 2018 The Cypherium Blockchain authors
+ *
+ * This file is part of the Cypherium Blockchain library.
+ *
+ * The Cypherium Blockchain library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Cypherium Blockchain library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Cypherium Blockchain library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-The protocol-file defines the actions that the protocol needs to do in each
-step. The root-node will call the `Start`-method of the protocol. Each
-node will only use the `Handle`-methods, and not call `Start` again.
-*/
+package cypherium
 
 import (
 	"crypto/sha256"
